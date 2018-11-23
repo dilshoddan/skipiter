fa351765358a8d3fec20ab729ae1b13b5889e476
Admin <admin@Mac-Admin.local>
2018-11-23 13:15:19 +0300

    Register view draft 9

--- Skipiter/Controller/RegisterViewController.swift
+++ Skipiter/Controller/RegisterViewController.swift
@@ -168,29 +168,4 @@ class RegisterViewController: UIViewController {
     
 }
 
-extension UIView {
-    func getSelectedTextField() -> UITextField? {
-        
-        let totalTextFields = getTextFieldsInView(view: self)
-        
-        for textField in totalTextFields{
-            if textField.isFirstResponder{
-                return textField
-            }
-        }
-        return nil
-    }
-    
-    func getTextFieldsInView(view: UIView) -> [UITextField] {
-        
-        var totalTextFields = [UITextField]()
-        
-        for subview in view.subviews as [UIView] {
-            if let textField = subview as? UITextField {
-                totalTextFields += [textField]
-            } else {
-                totalTextFields += getTextFieldsInView(view: subview)
-            }
-        }
-        return totalTextFields
-    }}
+
---
+++ Skipiter/View/Extensions.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  Skipiter
+//
+//  Created by Admin on 11/23/18.
+//  Copyright © 2018 Home. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView {
+    func getSelectedTextField() -> UITextField? {
+        
+        let totalTextFields = getTextFieldsInView(view: self)
+        
+        for textField in totalTextFields{
+            if textField.isFirstResponder{
+                return textField
+            }
+        }
+        return nil
+    }
+    
+    func getTextFieldsInView(view: UIView) -> [UITextField] {
+        
+        var totalTextFields = [UITextField]()
+        
+        for subview in view.subviews as [UIView] {
+            if let textField = subview as? UITextField {
+                totalTextFields += [textField]
+            } else {
+                totalTextFields += getTextFieldsInView(view: subview)
+            }
+        }
+        return totalTextFields
+    }
+}
