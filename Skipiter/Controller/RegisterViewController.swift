b9836553996b51e935cf7a82128c91c827770fed
dilshoddan <dilshoddan@gmail.com>
2018-11-26 17:49:07 +0500

    Register View MVC sorted

--- Skipiter/Controller/RegisterViewController.swift
+++ Skipiter/Controller/RegisterViewController.swift
@@ -11,33 +11,23 @@ import Stevia
 
 class RegisterViewController: UIViewController, UITextFieldDelegate {
     
-    private var registerView: UIStackView!
-    private var firstNameLabel: UILabel!
-    private var firstName: UITextField!
-    private var lastNameLabel: UILabel!
-    private var lastName: UITextField!
-    private var emailLabel: UILabel!
-    private var email: UITextField!
-    private var userNameLabel: UILabel!
-    private var userName: UITextField!
-    private var userPasswordLabel: UILabel!
-    private var userPassword: UITextField!
-    private var okButton: UIButton!
+    private var registerView: RegisterView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         SetControllerDefaults()
         render()
-        
-//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
-//        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(keyboardNotification), name: UIResponder.keyboardWillChangeFrameNotification,object: nil)
-        firstName.delegate = self
-        lastName.delegate = self
-        email.delegate = self
-        userName.delegate = self
-        userPassword.delegate = self
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardNotification),
+                                               name: UIResponder.keyboardWillChangeFrameNotification,
+                                               object: nil)
+        registerView.firstName.delegate = self
+        registerView.lastName.delegate = self
+        registerView.email.delegate = self
+        registerView.userName.delegate = self
+        registerView.userPassword.delegate = self
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -54,88 +44,13 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
         
         self.title = "RegisterVC"
         view.backgroundColor = ColorConstants.RegisterVC
-        
-        registerView = UIStackView()
-        registerView.backgroundColor = ColorConstants.RegisterVC
-        
-        firstNameLabel = UILabel()
-        firstNameLabel.text = "First name:"
-        firstName = UITextField()
-        
-        lastNameLabel = UILabel()
-        lastNameLabel.text = "Last name:"
-        lastName = UITextField()
-        
-        emailLabel = UILabel()
-        emailLabel.text = "Email address:"
-        email = UITextField()
-        
-        userNameLabel = UILabel()
-        userNameLabel.text = "User name:"
-        userName = UITextField()
-        
-        userPasswordLabel = UILabel()
-        userPasswordLabel.text = "User password:"
-        userPassword = UITextField()
-        userPassword.isSecureTextEntry = true
-        
-        //set default UITextField attributes
-        for textField in [firstName, lastName, email, userName, userPassword] {
-            textField?.backgroundColor = .white
-            textField?.borderStyle = .roundedRect
-            textField?.isEnabled = true
-            textField?.isUserInteractionEnabled = true
-        }
-        
-        
-        okButton = UIButton()
-        okButton.backgroundColor = ColorConstants.LoginViewVC
-        okButton.setTitle("OK", for: .normal)
-        okButton.tintColor = .white
-        okButton.layer.cornerRadius = 5
-        okButton.clipsToBounds = true
-        okButton.isEnabled = true
-        okButton.isUserInteractionEnabled = true
-        okButton.addTarget(self, action: #selector(OkClicked), for: .touchUpInside)
+        registerView = RegisterView(frame: view.bounds)
+        registerView.okButton.addTarget(self, action: #selector(OkClicked), for: .touchUpInside)
     }
     
     func render(){
-        
-        registerView.sv([firstNameLabel, firstName, lastNameLabel, lastName, emailLabel, email, userNameLabel, userName, userPasswordLabel, userPassword, okButton])
         view.sv(registerView)
-        registerView.height(100%).width(100%)
-        
-        let heightConstant = CGFloat(30)
-        let leftOffset = CGFloat(10)
-        
-        for label in [firstNameLabel, lastNameLabel, emailLabel, userNameLabel, userPasswordLabel] {
-            label?.height(heightConstant).left(leftOffset)
-        }
-        
-        for textField in [firstName, lastName, email, userName, userPassword] {
-            textField?.height(heightConstant).left(leftOffset)
-        }
-        okButton.height(heightConstant + 10).left(leftOffset)
-        
-        registerView.layout(
-            (2*heightConstant),
-            |-firstNameLabel-|,
-            |-firstName-|,
-            13,
-            |-lastNameLabel-|,
-            |-lastName-|,
-            13,
-            |-emailLabel-|,
-            |-email-|,
-            13,
-            |-userNameLabel-|,
-            |-userName-|,
-            13,
-            |-userPasswordLabel-|,
-            |-userPassword-|,
-            23,
-            |-okButton-|
-        )
+        registerView.height(100%).width(100%).centerInContainer()
         
     }
     
@@ -149,6 +64,7 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
                 }
                 else{
                     render()
+                    registerView.updateConstraints()
                 }
             }
         }
@@ -157,30 +73,6 @@ class RegisterViewController: UIViewController, UITextFieldDelegate {
             self.view.layoutIfNeeded()
         })
     }
-    
-    //    @objc func keyboardWillShow(notification: NSNotification) {
-    //        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-    //            let selectedTextField = registerView.getSelectedTextField()
-    //            if let selectedTextField = selectedTextField {
-    //                let selectedTextFieldBottomY = selectedTextField.frame.origin.y + selectedTextField.frame.size.height
-    //                if selectedTextFieldBottomY > keyboardSize.origin.y {
-    //                    registerView.frame.origin.y = registerView.frame.origin.y - (selectedTextFieldBottomY - keyboardSize.origin.y) - 5
-    //                }
-    //            }
-    //        }
-    //        UIView.animate(withDuration: 0.2, animations: { () -> Void in
-    //            self.registerView.layoutIfNeeded()
-    //            self.view.layoutIfNeeded()
-    //        })
-    //    }
-    //
-    //    @objc func keyboardWillHide(notification: NSNotification) {
-    //        render()
-    //        UIView.animate(withDuration: 0.2, animations: { () -> Void in
-    //            self.registerView.layoutIfNeeded()
-    //            self.view.layoutIfNeeded()
-    //        })
-    //    }
     
     
     
--- Skipiter/View/RegisterView.swift
+++ Skipiter/View/RegisterView.swift
@@ -7,15 +7,126 @@
 //
 
 import UIKit
+import Stevia
+
 
 class RegisterView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    private var shouldSetupConstraints: Bool!
+    public var registerView: UIStackView!
+    public var firstNameLabel: UILabel!
+    public var firstName: UITextField!
+    public var lastNameLabel: UILabel!
+    public var lastName: UITextField!
+    public var emailLabel: UILabel!
+    public var email: UITextField!
+    public var userNameLabel: UILabel!
+    public var userName: UITextField!
+    public var userPasswordLabel: UILabel!
+    public var userPassword: UITextField!
+    public var okButton: UIButton!
+
+    
+    override init(frame:CGRect){
+        super.init(frame:frame)
+        shouldSetupConstraints = true
+        SetControllerDefaults()
+        updateConstraints()
     }
-    */
+    
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+    }
+    
+    func SetControllerDefaults(){
+        
+        self.backgroundColor = ColorConstants.RegisterVC
+        
+        registerView = UIStackView()
+        registerView.backgroundColor = ColorConstants.RegisterVC
+        
+        firstNameLabel = UILabel()
+        firstNameLabel.text = "First name:"
+        firstName = UITextField()
+        
+        lastNameLabel = UILabel()
+        lastNameLabel.text = "Last name:"
+        lastName = UITextField()
+        
+        emailLabel = UILabel()
+        emailLabel.text = "Email address:"
+        email = UITextField()
+        
+        userNameLabel = UILabel()
+        userNameLabel.text = "User name:"
+        userName = UITextField()
+        
+        userPasswordLabel = UILabel()
+        userPasswordLabel.text = "User password:"
+        userPassword = UITextField()
+        userPassword.isSecureTextEntry = true
+        
+        //set default UITextField attributes
+        for textField in [firstName, lastName, email, userName, userPassword] {
+            textField?.backgroundColor = .white
+            textField?.borderStyle = .roundedRect
+            textField?.isEnabled = true
+            textField?.isUserInteractionEnabled = true
+        }
+        
+        
+        okButton = UIButton()
+        okButton.backgroundColor = ColorConstants.LoginViewVC
+        okButton.setTitle("OK", for: .normal)
+        okButton.tintColor = .white
+        okButton.layer.cornerRadius = 5
+        okButton.clipsToBounds = true
+        okButton.isEnabled = true
+        okButton.isUserInteractionEnabled = true
+        
+    }
+    
+    
+    override func updateConstraints(){
+        if(shouldSetupConstraints){
+            
+            registerView.sv([firstNameLabel, firstName, lastNameLabel, lastName, emailLabel, email, userNameLabel, userName, userPasswordLabel, userPassword, okButton])
+            self.sv(registerView)
+            registerView.height(100%).width(100%)
+            
+            let heightConstant = CGFloat(30)
+            let leftOffset = CGFloat(10)
+            
+            for label in [firstNameLabel, lastNameLabel, emailLabel, userNameLabel, userPasswordLabel] {
+                label?.height(heightConstant).left(leftOffset)
+            }
+            
+            for textField in [firstName, lastName, email, userName, userPassword] {
+                textField?.height(heightConstant).left(leftOffset)
+            }
+            okButton.height(heightConstant + 10).left(leftOffset)
+            
+            registerView.layout(
+                (2*heightConstant),
+                |-firstNameLabel-|,
+                |-firstName-|,
+                13,
+                |-lastNameLabel-|,
+                |-lastName-|,
+                13,
+                |-emailLabel-|,
+                |-email-|,
+                13,
+                |-userNameLabel-|,
+                |-userName-|,
+                13,
+                |-userPasswordLabel-|,
+                |-userPassword-|,
+                23,
+                |-okButton-|
+            )
+        }
+        super.updateConstraints()
+    }
 
 }
