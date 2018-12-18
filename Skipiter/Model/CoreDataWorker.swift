6b353d555a3ba8375097bca7d388d4c94372a9a0
Admin <admin@Mac-Admin.local>
2018-12-18 12:30:41 +0300

    Core Data refactoring

--- Skipiter/Model/CoreDataWorker.swift
+++ Skipiter/Model/CoreDataWorker.swift
@@ -11,9 +11,11 @@ import UIKit
 
 class CoreDataWorker {
     private var managedContext: NSManagedObjectContext!
+    private var usersFetchRequest: NSFetchRequest<NSFetchRequestResult>!
     
     init(appDelegate: AppDelegate) {
         managedContext = appDelegate.persistentContainer.viewContext
+        usersFetchRequest = NSFetchRequest.init(entityName: "Users")
     }
     
     func SetValuesTo(coreUser: NSManagedObject, fromUser: User){
@@ -22,6 +24,14 @@ class CoreDataWorker {
         coreUser.setValue(fromUser.email, forKey: "email")
         coreUser.setValue(fromUser.userName, forKey: "userName")
         coreUser.setValue(fromUser.userPassword, forKey: "userPassword")
+    }
+    
+    func CreateUserFor(coreUser: NSManagedObject) -> User {
+        return User(firstname: coreUser.value(forKey: "firstName") as! String,
+                    lastName: coreUser.value(forKey: "lastName") as! String,
+                    email: coreUser.value(forKey: "email") as! String,
+                    userName: coreUser.value(forKey: "userName") as! String,
+                    userPassword: coreUser.value(forKey: "userPassword") as! String)
     }
     
     func SaveUser(user: User){
@@ -41,12 +51,8 @@ class CoreDataWorker {
         var users = [User]()
         do {
             let result = try managedContext.fetch(fetchRequest)
-            for row in result {
-                let user  = User(firstname: row.value(forKey: "firstName") as! String,
-                                 lastName: row.value(forKey: "lastName") as! String,
-                                 email: row.value(forKey: "email") as! String,
-                                 userName: row.value(forKey: "userName") as! String,
-                                 userPassword: row.value(forKey: "userPassword") as! String)
+            for coreUser in result {
+                let user  = CreateUserFor(coreUser: coreUser)
                 users.append(user)
             }
         }
@@ -57,12 +63,32 @@ class CoreDataWorker {
         return users
     }
     
-    func Update(user: User, searchKey: String, searchValue: String){
-        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "Users")
-        fetchRequest.predicate = NSPredicate(format: "\(searchKey) = %@", "\(searchValue)")
+    func Delete(user: User){
+        usersFetchRequest.predicate = NSPredicate(format: "userName = %@", "\(user.userName)")
         
         do{
-            let fetchedUser = try managedContext.fetch(fetchRequest)
+            let fetchedUser = try managedContext.fetch(usersFetchRequest)
+            if fetchedUser.count > 0 {
+                let deleteUser = fetchedUser[0] as! NSManagedObject
+                managedContext.delete(deleteUser)
+                do {
+                    try managedContext.save()
+                }
+                catch {
+                    print("Could not delete the user: \(user) . Error: \(error)")
+                }
+            }
+        }
+        catch {
+            print("Could not find the user. Error: \(error)")
+        }
+    }
+    
+    func Update(user: User){
+        usersFetchRequest.predicate = NSPredicate(format: "userName = %@", "\(user.userName)")
+        
+        do{
+            let fetchedUser = try managedContext.fetch(usersFetchRequest)
             if fetchedUser.count > 0 {
                 let updateUser = fetchedUser[0] as! NSManagedObject
                 SetValuesTo(coreUser: updateUser, fromUser: user)
@@ -73,19 +99,18 @@ class CoreDataWorker {
         }
     }
     
-    func UpdateProfileImageOf(user: User, _ key: String){
-        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "Users")
-        fetchRequest.predicate = NSPredicate(format: "userName= %@", "\(user.userName)")
+    func UpdateProfileImageOf(user: User, _ withKey: String){
+        usersFetchRequest.predicate = NSPredicate(format: "userName= %@", "\(user.userName)")
         
         do{
-            let fetchedUser = try managedContext.fetch(fetchRequest)
+            let fetchedUser = try managedContext.fetch(usersFetchRequest)
             if fetchedUser.count > 0 {
                 let updateUser = fetchedUser[0] as! NSManagedObject
-                if key == "profileImage" {
-                    updateUser.setValue(user.profileImage?.jpegData(compressionQuality: 1.0)!, forKey: key)
+                if withKey == "profileImage" {
+                    updateUser.setValue(user.profileImage?.jpegData(compressionQuality: 1.0)!, forKey: withKey)
                 }
-                else if key == "profileBanner" {
-                    updateUser.setValue(user.profileBanner?.jpegData(compressionQuality: 1.0)!, forKey: key)
+                else if withKey == "profileBanner" {
+                    updateUser.setValue(user.profileBanner?.jpegData(compressionQuality: 1.0)!, forKey: withKey)
                 }
                 
             }
@@ -96,26 +121,21 @@ class CoreDataWorker {
     }
     
     func IsAuthenticated(userName: String, userPassword: String) -> User? {
-        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "Users")
-        fetchRequest.predicate = NSPredicate(format: "userName = %@", "\(userName)")
+        usersFetchRequest.predicate = NSPredicate(format: "userName = %@", "\(userName)")
         var returnUser: User!
         do {
-            let fetchedUser = try managedContext.fetch(fetchRequest)
+            let fetchedUser = try managedContext.fetch(usersFetchRequest)
             if fetchedUser.count > 0 {
-                let user = fetchedUser[0] as! NSManagedObject
-                let fetchedUserPassword = user.value(forKey: "userPassword") as! String
+                let coreUser = fetchedUser[0] as! NSManagedObject
+                let fetchedUserPassword = coreUser.value(forKey: "userPassword") as! String
                 if fetchedUserPassword == userPassword {
-                    returnUser = User(firstname: user.value(forKey: "firstName") as! String,
-                                      lastName: user.value(forKey: "lastName") as! String,
-                                      email: user.value(forKey: "email") as! String,
-                                      userName: user.value(forKey: "userName") as! String,
-                                      userPassword: user.value(forKey: "userPassword") as! String)
+                    returnUser = CreateUserFor(coreUser: coreUser)
                     
-                    let profileImage = user.value(forKey: "profileImage") as? Data
+                    let profileImage = coreUser.value(forKey: "profileImage") as? Data
                     if let profileImage = profileImage {
                         returnUser.profileImage = UIImage(data: profileImage)
                     }
-                    let profileBanner = user.value(forKey: "profileBanner") as? Data
+                    let profileBanner = coreUser.value(forKey: "profileBanner") as? Data
                     if let profileBanner = profileBanner {
                         returnUser.profileBanner = UIImage(data: profileBanner)
                     }
@@ -129,10 +149,9 @@ class CoreDataWorker {
     }
     
     func IsUnique(userName: String) -> Bool{
-        let fetchRequest: NSFetchRequest<NSFetchRequestResult> = NSFetchRequest.init(entityName: "Users")
-        fetchRequest.predicate = NSPredicate(format: "userName = %@", "\(userName)")
+        usersFetchRequest.predicate = NSPredicate(format: "userName = %@", "\(userName)")
         do {
-            let fetchedUser = try managedContext.fetch(fetchRequest)
+            let fetchedUser = try managedContext.fetch(usersFetchRequest)
             if fetchedUser.count == 0 {
                 return true
             }
